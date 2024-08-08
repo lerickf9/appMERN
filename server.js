3506499d8a690e96088b3120456357047f972038
Erick <efuentes.edf9@gmail.com>
2024-08-07 22:37:48 -0400

    Add db mongoose

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connect from "./db/connect.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+//midleware
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const server = async () => {
-    console.log("server is running");
+    try{
+        await connect();
+
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch( error){
+        console.log("Fialed to start server...", error.message);
+        process.exit(1);
+    }
 };
 
 server();
